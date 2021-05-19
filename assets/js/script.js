f2f311843249f5cf9d9c95264a9215539ce82a03
Gustavo Jaen <jaengustavof@gmail.com>
2021-05-19 20:09:53 +0200

    Responsive 1680 y 1600

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-      $( document ).ready(function() {
+$( document ).ready(function() {
 
-        $(".container").scroll(function(){
-          console.log(this.scrollLeft)
-        });
+    $(".container").scroll(function(){
+      console.log(this.scrollLeft)
+    });
+    let executed = false;
+    let deviceWith = $(window).innerWidth();
+    console.log("Device Width: "+deviceWith)
         
-     $(function() {
-      
-      let sectionPosition = 1;
-      let sectionLeft = 0;
-      let secciones = document.getElementsByTagName("section");
-      var executed = false;
+    $(function() {
 
       function wwa1(){
         var span1 = $(".spanText1");
@@ -22,6 +20,7 @@
               time +=400;
             }
       }
+
       function wwa2(){
        $(".secondTitle").css("opacity", "1");
         $(".secondTitle").css("left", "0");
@@ -35,6 +34,7 @@
               time +=400;
           }
       }
+
       function wwa3(){
         $(".thirdTitle").css("opacity", "1");
         $(".thirdTitle").css("left", "0");
@@ -77,8 +77,15 @@
         $("#wwAreButton").click(function(){
           menuSelected()
           $(this).addClass("linkSelected");
+          if (deviceWith >1680) {
+            $(".container").scrollLeft(1343);            
+          }else if (deviceWith <= 1680 && deviceWith>1600){
+            $(".container").scrollLeft(1483);
+          }else if (deviceWith <= 1600){
+            $(".container").scrollLeft(1400);
+          }
 
-          $(".container").scrollLeft(1343);
+
           $(".logo").css("opacity", 1);
           wwa1();
           setInterval(function(){
@@ -134,80 +141,9 @@
         });
         //Menu//
 
-
-         $(".container").mousewheel(function(event, delta) {
-
-          //Scroll Automatico///
-          /*  if(delta == -1){
-              if(sectionPosition < 4){ 
-                sectionLeft += -100;
-                  $("section").css("left", sectionLeft+"%"); 
-                  sectionPosition++;
-                }
-                
-                event.preventDefault();
-            }else if(delta == 1){
-              if(sectionPosition > 1){
-                sectionLeft += +100;
-                $("section").css("left", sectionLeft+"%"); 
-                sectionPosition--;
-              }
-            }
-            */    
-          //Scroll Manual  
-          if(this.scrollLeft < 1200){
-            menuSelected();
-          }else if(this.scrollLeft >= 1343 && this.scrollLeft < 3063){
-            menuSelected();
-            $(menuLink[0]).addClass("linkSelected");
-          }else if(this.scrollLeft >= 3063 && this.scrollLeft < 5083){
-            menuSelected();
-            $(menuLink[1]).addClass("linkSelected");
-          }else if(this.scrollLeft >= 5083 && this.scrollLeft < 7003){
-            menuSelected();
-            $(menuLink[2]).addClass("linkSelected");
-          }else if(this.scrollLeft >= 7003){
-            menuSelected();
-            $(menuLink[3]).addClass("linkSelected");
-          } 
-
-           this.scrollLeft -= (delta * 200);
-           //console.log(delta);
-            event.preventDefault();
-            //console.log(this.scrollLeft);
-
-
-        //Scroll Effect Section 1.2//
-            if(this.scrollLeft >= 500) {
-              wwa1();
-            }
-
-            if(this.scrollLeft >= 800) {
-              wwa2();
-            }
-
-            if(this.scrollLeft >= 1100) {
-              wwa3()
-            }
-
-            //One Time Function for Section 2//
-            if( this.scrollLeft >=2300){
-              deploy();
-            }
-
-            //Logo fadeOut Second Section
-            if(this.scrollLeft > 2800 && this.scrollLeft <= 5000){
-              $(".logo").css("opacity", 0);
-            }else{
-              $(".logo").css("opacity", 1);
-            }
-
-         });
-
-         //Logo Animation //
-
+        //Logo Animation //
           var targets = $(".target");
-            var vel = 100;
+          var vel = 100;
             for(let i =0; i<=targets.length; i++){
                 setTimeout(function(){ 
                // console.log(targets[i]);             
@@ -240,8 +176,76 @@
             },3500);//Logo Animation //
 
 
-         //main Section//
-   
+         $(".container").mousewheel(function(event, delta) {
+
+          //Scroll Manual  
+          this.scrollLeft -= (delta * 200);
+          //console.log(delta);
+          event.preventDefault();
+          console.log(this.scrollLeft);
+
+          //Menu Link Highlight
+          if(deviceWith >1600){
+            if(this.scrollLeft < 1200){
+              menuSelected();
+            }else if(this.scrollLeft >= 1343 && this.scrollLeft < 3063){
+              menuSelected();
+              $(menuLink[0]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 3063 && this.scrollLeft < 5083){
+              menuSelected();
+              $(menuLink[1]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 5083 && this.scrollLeft < 7003){
+              menuSelected();
+              $(menuLink[2]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 7003){
+              menuSelected();
+              $(menuLink[3]).addClass("linkSelected");
+            } 
+          }else if(deviceWith <=1600){
+              if(this.scrollLeft < 1400){
+              menuSelected();
+            }else if(this.scrollLeft >= 1400 && this.scrollLeft < 2839){
+              menuSelected();
+              $(menuLink[0]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 2839 && this.scrollLeft < 4439){
+              menuSelected();
+              $(menuLink[1]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 4439 && this.scrollLeft < 6039){
+              menuSelected();
+              $(menuLink[2]).addClass("linkSelected");
+            }else if(this.scrollLeft >= 6039){
+              menuSelected();
+              $(menuLink[3]).addClass("linkSelected");
+            }
+          }
+          
+          // END Menu Link Highlight
+
+
+        //Scroll Effect Section 1.2//
+            if(this.scrollLeft >= 500) {
+              wwa1();
+            }
+            if(this.scrollLeft >= 800) {
+              wwa2();
+            }
+            if(this.scrollLeft >= 1100) {
+              wwa3()
+            }
+
+            //One Time Function for Section 2//
+            if( this.scrollLeft >=2300){
+              deploy();
+            }
+            //Logo fadeOut Second Section
+            if(this.scrollLeft > 2800 && this.scrollLeft <= 5000){
+              $(".logo").css("opacity", 0);
+            }else{
+              $(".logo").css("opacity", 1);
+            }
+         });
+
+        //main Section//  
         setTimeout(function(){          
             $(".wrapper").css("opacity", 1);
         },4200);
@@ -256,87 +260,129 @@
           $(".firstTitle").css("left", "0");
         }, 4900);
 
-        //Second Section//
-        var indexKnow = $(".indexKnow");
-        var moreToKnow = $(".moreToKnow");
+//////////Second Section//////////
+//////////Second Section//////////
+//////////Second Section//////////
+        let indexKnow = $(".indexKnow");
+        let moreToKnow = $(".moreToKnow");
+        let indexText = $(".indexText");
+        let indexTextSpan = $(".indexTextSpan");
+        let indexTextPar = $(".indexTextPar");
        // console.log(indexKnow);
+        function cssTop(two, three, four){
 
+            $(".secTwo").css("top", two+"px");
+            $(".secThree").css("top", three+"px");
+            $(".secFour").css("top", four+"px");
+        }
         function cero() {
           for(index of indexKnow){
             $(indexKnow[0]).removeClass("indexNone");
             $(indexKnow).css("opacity",1);
           }
           $(indexKnow[0]).css("opacity",0);
-          $(".secTwo").css("top", "706px");
-          $(".secThree").css("top", "756px");
-          $(".secFour").css("top", "806px");
+          if(deviceWith >1680){          
+            cssTop(706, 756, 806);
+          }else if(deviceWith<=1680 && deviceWith >1600){
+            cssTop(686, 736, 786);
+          }else if(deviceWith<=1600){
+            cssTop(592, 627, 662);
+          }
         }
-
         function uno() {
           for(index of indexKnow){
             $(indexKnow[0]).removeClass("indexNone");
             $(indexKnow).css("opacity",1);
           }
           $(indexKnow[1]).css("opacity",0);
-          $(".secTwo").css("top", "50px");
-          $(".secThree").css("top", "756px");
-          $(".secFour").css("top", "806px");
+          if(deviceWith >1680){          
+            cssTop(50, 756, 806);
+          }else if(deviceWith<=1680 && deviceWith >1600){
+            cssTop(50, 736, 786);
+          }else if(deviceWith<=1600){
+            cssTop(35, 627, 662);
+          }
         }
-
         function dos() {
           for(index of indexKnow){
             $(indexKnow[0]).removeClass("indexNone");
             $(indexKnow).css("opacity",1);
           }
           $(indexKnow[2]).css("opacity",0);
-          $(".secTwo").css("top", "50px");
-          $(".secThree").css("top", "100px");
-          $(".secFour").css("top", "806px");
+          if(deviceWith >1680){          
+            cssTop(50, 100, 806);
+          }else if(deviceWith<=1680 && deviceWith >1600){
+            cssTop(50, 100, 786);
+          }else if(deviceWith<=1600){
+            cssTop(35, 70, 662);
+          }
         }
-
         function tres(){
           for(index of indexKnow){
             $(indexKnow[0]).removeClass("indexNone");
             $(indexKnow).css("opacity",1);
-          }
+          }        
           $(indexKnow[3]).css("opacity",0);
-          $(".secTwo").css("top", "50px");
-          $(".secThree").css("top", "100px");
-          $(".secFour").css("top", "150px");
+          if(deviceWith>1600){
+            cssTop(50, 100, 150);
+          }else if (deviceWith <= 1600){
+            cssTop(35, 70, 105);
+          }
         }
 
         indexKnow.click(function(event){
-          var result = Array.from(moreToKnow).indexOf(event.target);
-          var result2 = Array.from(indexKnow).indexOf(event.target);
-
+          let result = Array.from(moreToKnow).indexOf(event.target);
+          let result2 = Array.from(indexKnow).indexOf(event.target);
           for(index of indexKnow){
             $(indexKnow[0]).removeClass("indexNone");
             $(indexKnow).css("opacity",1);
           }
-
           if(result == 0 || result2 ==0) {
             cero();
             cont=0;
           }
-
           if(result == 1 || result2 ==1) {
             uno();
             cont=1;
           }
-
           if(result == 2 || result2 ==2) {
             dos();
             cont=2;
           }
-
           if(result == 3 || result2 ==3) {
             tres();
             cont=3;
           }
-
-        });         
+        });  
+        indexText.click(function(event){
+          let result3 = Array.from(indexText).indexOf(event.target);
+          let result4 = Array.from(indexTextSpan).indexOf(event.target); 
+          let result5 = Array.from(indexTextPar).indexOf(event.target); 
+           for(index of indexKnow){
+            $(indexKnow[0]).removeClass("indexNone");
+            $(indexKnow).css("opacity",1);
+          }
+          if(result3 == 0 || result4 == 0 || result5 == 0) {
+            cero();
+            cont=0;
+          }
+          if(result3 == 1 || result4 == 1 || result5 == 1) {
+            uno();
+            cont=1;
+          }
+          if(result3 == 2 || result4 == 2 || result5 == 2) {
+            dos();
+            cont=2;
+          }
+          if(result3 == 3 || result4 == 3 || result5 == 3) {
+            tres();
+            cont=3;
+          }
+        })       
         tres();
-        //Second Section//
+//////////Second Section//////////
+//////////Second Section//////////
+//////////Second Section//////////
 
         //Third Section//
         var pixels = 0;
@@ -374,7 +420,9 @@
           $(".buttonText").text("Why join us");         
         });
 
-        // Fourth Section //
+////// Fourth Section //////
+////// Fourth Section //////
+////// Fourth Section //////
           var fourth = 1;
           var fourthSecIndex = $(".fourthSecIndex");
           var fourthIndexName = $(".fourthIndexName");
@@ -401,8 +449,14 @@
           $(fourthSectionText[0]).css("display", "flex");
           $(fourthSecIndex[0]).css("width", "116px");
           $(fourthSecIndex[0]).css("background", "rgba(0,0,0,0.5)");
-          $(fSectionDinamicTitle[0]).css("fontSize", "50px");
-          $(fSectionDinamicTitle[0]).css("lineHeight", "60px");
+          if(deviceWith >1600){
+            $(fSectionDinamicTitle[0]).css("fontSize", "50px");
+            $(fSectionDinamicTitle[0]).css("lineHeight", "60px");
+          }else if(deviceWith <=1600){
+            $(fSectionDinamicTitle[0]).css("fontSize", "40px");
+            $(fSectionDinamicTitle[0]).css("lineHeight", "50px");
+          }
+
 
           let backImg0 = "url('assets/rsc/img/section4back1.webp')";
           let backImg1 = "url('assets/rsc/img/section4back2.webp')";
@@ -420,10 +474,15 @@
               $(fourthSecIndex[index]).css("width", "116px");
               $(fourthSecIndex[index]).css("background", "rgba(0,0,0,0.5)");
               $(fourthSectionText[index]).css("display", "flex");
-              $(fSectionDinamicTitle[index]).css("fontSize", "50px");
-              $(fSectionDinamicTitle[index]).css("lineHeight", "60px");
-
+              if(deviceWith >1600){
+                $(fSectionDinamicTitle[index]).css("fontSize", "50px");
+                $(fSectionDinamicTitle[index]).css("lineHeight", "60px");
+              }else if(deviceWith <=1600){
+                $(fSectionDinamicTitle[index]).css("fontSize", "40px");
+                $(fSectionDinamicTitle[index]).css("lineHeight", "50px");
+              }
               $(".fourthSectionContent").css("background", arrImg[index]);
+
           }
 
           setInterval(function(){
